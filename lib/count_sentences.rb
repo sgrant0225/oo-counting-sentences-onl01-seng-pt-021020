da4169db1b5acb35368216efd4ae5db1b7cf3cb4
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-28 02:37:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+     true 
+   else 
+     false
+   end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else
+      false 
+    end
   end
 
   def count_sentences
-
-  end
-end
+      new_sentence = self.split(/[.!?]/).reject {|value| value.empty?}
+      binding.pry
+      return new_sentence.size
+      end
+    end 
+  
